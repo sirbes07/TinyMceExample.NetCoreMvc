f5d76ad021b89dff92caa5359c7be4f891d0430c
Emre Sogut <emre.sogut@rktrzm.local>
2020-03-27 13:37:18 +0300

    Add project files.

---
+++ TinyWebApp/Controllers/ContentController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TinyWebApp.Models;
+
+namespace TinyWebApp.Controllers
+{
+    public class ContentController : Controller
+    {
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost, ActionName("Create")]
+        public IActionResult Create(ContentsModel model)
+        {
+
+            ViewBag.HtmlContent = model.Content;
+
+            return View(model);
+        }
+    }
+}
---
+++ TinyWebApp/Models/ContentsModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace TinyWebApp.Models
+{
+    public class ContentsModel
+    {
+        public string Title { get; set; }
+        public DateTime PostedOn { get; set; }
+        public string Tags { get; set; }
+
+        [AllowHtml]
+        public string Content { get; set; }
+    }
+}
